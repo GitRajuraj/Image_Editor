ba194ed404d5ba441d143232def2817538f4f035
GitRajuraj <rajuraj.kalita@reverieinc.com>
2016-01-22 17:20:24 +0530

    Added Save Button

--- MyApplication/app/src/main/java/iamraju/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/iamraju/myapplication/MainActivity.java
@@ -3,24 +3,31 @@ package iamraju.myapplication;
 import android.app.Activity;
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.graphics.ColorFilter;
+import android.graphics.LightingColorFilter;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
-import android.graphics.ColorFilter;
-import android.graphics.LightingColorFilter;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.Random;
 
 
 public class MainActivity extends Activity {
 
     static ImageView imageView;
     SeekBar mybar;
+    Bitmap bm;
     private static int RESULT_LOAD_IMAGE = 1;
 
 
@@ -51,32 +58,41 @@ public class MainActivity extends Activity {
                                           boolean fromUser) {
 
 
-                ColorFilter filter1 = new LightingColorFilter(Color.WHITE, Color.BLACK);
+                ColorFilter filter1 = new LightingColorFilter(Color.WHITE, Color.BLUE);
                 ColorFilter filter2 = new LightingColorFilter(Color.GREEN, Color.BLACK);
                 ColorFilter filter3 = new LightingColorFilter(Color.RED, Color.BLACK);
 
 
                 if (imageView != null) {
 
-                    if (progress >=0 && progress < 25)
+                    if (progress >=0 && progress < 25) {
                         imageView.setColorFilter(0);
-
-                    if (progress >=25 && progress < 50)
+                        imageView.buildDrawingCache();
+                        bm = imageView.getDrawingCache();
+                    }
+                    if (progress >=25 && progress < 50) {
                         imageView.setColorFilter(filter1);
+                        imageView.buildDrawingCache();
+                        bm = imageView.getDrawingCache();
+                    }
 
-
-                    if (progress >= 50 && progress < 75)
+                    if (progress >= 50 && progress < 75) {
                         imageView.setColorFilter(filter2);
-
-                    if (progress >= 75 && progress < 100)
+                        imageView.buildDrawingCache();
+                        bm = imageView.getDrawingCache();
+                    }
+                    if (progress >= 75 && progress < 100) {
                         imageView.setColorFilter(filter3);
-
+                        imageView.buildDrawingCache();
+                        bm = imageView.getDrawingCache();
+                    }
                 }
             }
         });
 
 
         Button buttonLoadImage = (Button) findViewById(R.id.buttonLoadPicture);
+
 
         buttonLoadImage.setOnClickListener(new View.OnClickListener() {
 
@@ -90,6 +106,23 @@ public class MainActivity extends Activity {
                 startActivityForResult(i, RESULT_LOAD_IMAGE);
             }
         });
+
+
+
+        Button SaveImage = (Button) findViewById(R.id.Save_Picture);
+
+        SaveImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                SaveImage(bm);
+
+            }
+        });
+
+
+
+
     }
 
 
@@ -115,5 +148,27 @@ public class MainActivity extends Activity {
         }
 
 
+    }
+
+    private void SaveImage(Bitmap finalBitmap) {
+
+        String root = Environment.getExternalStorageDirectory().toString();
+        File myDir = new File(root + "/saved_images");
+        myDir.mkdirs();
+        Random generator = new Random();
+        int n = 10000;
+        n = generator.nextInt(n);
+        String fname = "Image-"+ n +".jpg";
+        File file = new File (myDir, fname);
+        if (file.exists ()) file.delete ();
+        try {
+            FileOutputStream out = new FileOutputStream(file);
+            finalBitmap.compress(Bitmap.CompressFormat.JPEG, 90, out);
+            out.flush();
+            out.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
