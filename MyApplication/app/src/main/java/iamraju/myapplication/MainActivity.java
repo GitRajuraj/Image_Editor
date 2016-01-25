0daf539b8394c7fdb79329806e3d1326385d5dd9
GitRajuraj <rajuraj.kalita@reverieinc.com>
2016-01-25 16:57:49 +0530

    Updated Maximum Things

--- MyApplication/app/src/main/java/iamraju/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/iamraju/myapplication/MainActivity.java
@@ -1,13 +1,11 @@
 package iamraju.myapplication;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Color;
-import android.graphics.ColorFilter;
-import android.graphics.LightingColorFilter;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -15,8 +13,9 @@ import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ImageView.ScaleType;
+import android.widget.LinearLayout;
 import android.widget.SeekBar;
-import android.widget.SeekBar.OnSeekBarChangeListener;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -25,6 +24,7 @@ import java.util.Random;
 
 public class MainActivity extends Activity {
 
+    Context context;
     static ImageView imageView;
     SeekBar mybar;
     Bitmap bm;
@@ -37,62 +37,67 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
 
-        mybar = (SeekBar) findViewById(R.id.seekBar1);
 
-        mybar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+//        mybar = (SeekBar) findViewById(R.id.seekBar1);
+//
+//        mybar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+//
+//            @Override
+//            public void onStopTrackingTouch(SeekBar seekBar) {
+//
+//                //add here your implementation
+//            }
+//
+//            @Override
+//            public void onStartTrackingTouch(SeekBar seekBar) {
+//
+//                //add here your implementation
+//            }
+//
+//            @Override
+//            public void onProgressChanged(SeekBar seekBar, int progress,
+//                                          boolean fromUser) {
+//
+//
+//                ColorFilter filter1 = new LightingColorFilter(Color.TRANSPARENT, Color.BLUE);
+//                ColorFilter filter2 = new LightingColorFilter(Color.GREEN, Color.BLACK);
+//                ColorFilter filter3 = new LightingColorFilter(Color.RED, Color.BLACK);
+//
+//
+//                if (imageView != null) {
+//
+//                    if (progress >=0 && progress < 25) {
+//                        imageView.setColorFilter(0);
+//                        imageView.buildDrawingCache();
+//                        bm = imageView.getDrawingCache();
+//                    }
+//                    if (progress >=25 && progress < 50) {
+//                        imageView.setColorFilter(filter1);
+//                        imageView.buildDrawingCache();
+//                        bm = imageView.getDrawingCache();
+//                    }
+//
+//                    if (progress >= 50 && progress < 75) {
+//                        imageView.setColorFilter(filter2);
+//                        imageView.buildDrawingCache();
+//                        bm = imageView.getDrawingCache();
+//                    }
+//                    if (progress >= 75 && progress < 100) {
+//
+//                        imageView.setColorFilter(filter3);
+//                        imageView.buildDrawingCache();
+//                        bm = imageView.getDrawingCache();
+//
+//                    }
+//                }
+//            }
+//        });
 
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
-                //add here your implementation
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-                //add here your implementation
-            }
-
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress,
-                                          boolean fromUser) {
 
 
-                ColorFilter filter1 = new LightingColorFilter(Color.WHITE, Color.BLUE);
-                ColorFilter filter2 = new LightingColorFilter(Color.GREEN, Color.BLACK);
-                ColorFilter filter3 = new LightingColorFilter(Color.RED, Color.BLACK);
-
-
-                if (imageView != null) {
-
-                    if (progress >=0 && progress < 25) {
-                        imageView.setColorFilter(0);
-                        imageView.buildDrawingCache();
-                        bm = imageView.getDrawingCache();
-                    }
-                    if (progress >=25 && progress < 50) {
-                        imageView.setColorFilter(filter1);
-                        imageView.buildDrawingCache();
-                        bm = imageView.getDrawingCache();
-                    }
-
-                    if (progress >= 50 && progress < 75) {
-                        imageView.setColorFilter(filter2);
-                        imageView.buildDrawingCache();
-                        bm = imageView.getDrawingCache();
-                    }
-                    if (progress >= 75 && progress < 100) {
-                        imageView.setColorFilter(filter3);
-                        imageView.buildDrawingCache();
-                        bm = imageView.getDrawingCache();
-                    }
-                }
-            }
-        });
 
 
         Button buttonLoadImage = (Button) findViewById(R.id.buttonLoadPicture);
-
 
         buttonLoadImage.setOnClickListener(new View.OnClickListener() {
 
@@ -115,19 +120,17 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View v) {
 
-                SaveImage(bm);
-
+                if(imageView!=null)
+                    SaveImage(bm);
             }
         });
-
-
-
 
     }
 
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
         super.onActivityResult(requestCode, resultCode, data);
 
         if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
@@ -143,11 +146,26 @@ public class MainActivity extends Activity {
             cursor.close();
 
             imageView = (ImageView) findViewById(R.id.imgView);
-            imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+
+
+            LinearLayout layout = (LinearLayout) findViewById(R.id.linear);
+            for (int i = 0; i < 10; i++) {
+                ImageView imageView = new ImageView(this);
+                imageView.setId(i);
+                imageView.setPadding(1, 1, 1, 1);
+                imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+                imageView.setScaleType(ScaleType.FIT_XY);
+                layout.addView(imageView);
+            }
+
+
+
+
+//            imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+//            imageView.setBackgroundColor(Color.BLACK);
+
 
         }
-
-
     }
 
     private void SaveImage(Bitmap finalBitmap) {
